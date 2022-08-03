961e554de2479c32be036b7ccf882b9a1f4d3a67
chikaishiro <gran_turismo@126.com>
2022-08-04 02:03:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,10 @@ def main():
   data = "something"
   # some comments
   env = StreamExecutionEnvironment.get_execution_environment()
-  parr_num = 4
-  env.set_parallelism(parr_num)
-  t_env = StreamTableEnvironment.create(env)
-  t_env.exec("method",data)
+  parr_num = get_parrnum()
+  if parr_num != 0:
+    env.set_parallelism(parr_num)
+    t_env = StreamTableEnvironment.create(env)
+    t_env.exec("method",data)
 
   # this is from dev branch
